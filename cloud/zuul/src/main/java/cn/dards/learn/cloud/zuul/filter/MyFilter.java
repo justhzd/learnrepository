d27590f567e5e5cb743f6954bf777c36e0547eda
dard <justhzd@qq.com>
2018-09-04 22:01:19 +0800

    commit

---
+++ cloud/zuul/src/main/java/cn/dards/learn/cloud/zuul/filter/MyFilter.java
@@ -0,0 +1,49 @@
+package cn.dards.learn.cloud.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        System.out.println(request.getMethod() + ">>>" + request.getRequestURL().toString());
+        Object accessToken = request.getParameter("token");
+        if (accessToken == null) {
+            System.out.println("token is empty");
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        } else {
+            System.out.println("OK!");
+        }
+        return null;
+    }
+}
